e825d79a1d6b23213625592eb5a9d2c60bdba45d
asinha94 <anuraagsinha@hotmail.com>
2017-02-17 22:45:39 -0800

    Fixed comment. This is more of a test

--- configuration/setup.sh
+++ configuration/setup.sh
@@ -5,7 +5,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 
-# Add ROS Key to list
+# Add Package Keys to list
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 
