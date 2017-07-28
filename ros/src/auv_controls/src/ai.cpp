2c1d00c0c8938539e2266479e08c4a965c9647aa
asinha94 <asinha94@gmail.com>
2017-07-28 02:56:51 -0700

    added initial rev if ai

--- ros/src/auv_controls/src/ai.cpp
+++ ros/src/auv_controls/src/ai.cpp
@@ -1,8 +1,35 @@
 #include <ros/ros.h>
+#include "motor_controller/position.h"
+#include "vision/enable_detection.h"
 
-class AI 
+class ai_handler 
 {
+public:
+    ai_handler():
+    updated(false),
+    pos(object_position())
+    {}
 
+    bool hasUpdated()
+    {
+        return this->updated;
+    }
+
+    void updatePosition(const motor_controller::position pos)
+    {
+        updated = true;
+        this->pos = pos;
+    }
+
+    motor_controller::position getPosition()
+    {
+        this->updated = false;
+        return this->pos;
+    }
+
+private:
+    bool updated;
+    motor_controller::position pos;
 
 };
 
@@ -10,6 +37,17 @@ int main(int argc, char ** argv)
 {
     ros::init(argc, argv, "ai");
     ros::NodeHandle nh("~");
+    ai_handler ai_handle;
+    /*
+    Make a for every vision node we are using
+    ros::ServiceClient enableNodeA = nh.serviceClient<vision::enable_detection>("/reciever/enableProcessing");
+    ros::Subscriber sub = nh.subscribe<motor_controller::position>("/nodeA/position", 20, &ai_handler::ai_handler, &ai_handle);
+
+    */
+
+    // Start off by doing a quick sweep of the surroundings
+    // Enable colout detector
+    
 
     return 0;
 }
